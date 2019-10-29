3c1db3a1a8b2638f54c92d81f6034991d61eab99
Nathan Gerday <nat.gerday@gmail.com>
2019-10-29 13:07:22 +0100

    Working inject_letter

--- Auteur.py
+++ Auteur.py
@@ -17,7 +17,7 @@ class Auteur(Acteur):
         self.letters_bag = []
         self.current_period = 0
         self.head_block = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        self.main_loop() # TODO Uncomment once main loop is defined
+        self.main_loop()
 
     def main_loop(self):
         self.start()
@@ -25,7 +25,6 @@ class Auteur(Acteur):
         self.injectLetter(self.generateLetter(), self.current_period, self.head_block, self.pkstr)
         self.stop()
         #Define main  routine here
-        pass
 
     def generateLetter(self):
         if(len(self.letters_bag) > 0):
@@ -36,41 +35,37 @@ class Auteur(Acteur):
     def byte_to_binary(self, n):
         return ''.join(str((n & (1 << i)) and 1) for i in reversed(range(8)))
     
-    def injectLetter(self,letter,period,hash_pred,author):
+    def injectLetter(self, letter, period, hash_pred, author):
 
-        if(len(letter) == 1 ):
-                x = {
-                    "letter" : letter,
-                    "period" : period, #taille du message 8 bytes qui encodent la taille en binaire
-                    "head" : hash_pred,
-                    "author" :  author      #self.pk.encode(encoder=nae.HexEncoder)
-                }
+        if(len(letter) == 1):
+            to_inject = {
+                "letter" : letter,
+                "period" : period, #taille du message 8 bytes qui encodent la taille en binaire
+                "head" : hash_pred,
+                "author" :  author      #self.pk.encode(encoder=nae.HexEncoder)
+            }
 
-                letter_bin = bin(ord(letter)).lstrip("0b").zfill(8)
-                letter_bin = bytes(letter, "utf-8")
-                print(letter_bin)
-                period_bin = "{0:b}".format(period).zfill(64) # TODO Not sure if that is big endian order
-                period_bin = bytes(period)
-                print(period_bin)
-                head_bin = bin(int(hash_pred, 16)).lstrip("0b").zfill(256)
-                head_bin = bytes(hash_pred, "utf-8")
-                print(head_bin)
-                author_bin = bin(int(author, 16)).lstrip('0b').zfill(256)
-                author_bin = bytes(author, "utf-8")
-                print(author_bin)
+            hasher = hashlib.sha256()
 
-                concat = letter_bin + period_bin + head_bin + author_bin
-                m = hashlib.sha256()
-                m.update(concat)
-                res_concat = m.hexdigest()
-                print(res_concat)
-                sig = crypto.sign(self.sk, res_concat.encode())
-                print(sig)
-                print(crypto.verify(self.pkstr, res_concat.encode(), crypto.sign(self.sk, res_concat.encode())))
+            letter_bin = ord(letter).to_bytes(1, byteorder="big")
+            hasher.update(letter_bin)
+            
+            period_bin = (period).to_bytes(8, byteorder="big")
+            hasher.update(period_bin)
+            
+            head_bin = int(hash_pred, 16).to_bytes(32, byteorder="big")
+            hasher.update(head_bin)
+            
+            author_bin = int(author, 16).to_bytes(32, byteorder="big")
+            hasher.update(author_bin)
+            
 
-                x["signature"] = sig
-                print(x)
-                server_coms.inject_letter(self.socket, x)
+            res_concat = hasher.digest()
+            sig = crypto.sign(self.sk, res_concat)
+            to_inject["signature"] = sig
+            print(to_inject)
+
+            server_coms.inject_letter(self.socket, to_inject)
 
     # TODO ==== Define how to handle server responses in this class here ====
     # TODO Pay attention to criticals sections
